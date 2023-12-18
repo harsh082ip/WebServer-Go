aadeee141f61bae91ceef032d0fe242cdc9f57eb
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-12-18 20:30:26 +0530

    Created Web Server

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not Supported", http.StatusNotFound)
+		return
+	}
+	fmt.Println(w, r)
+	fmt.Fprintf(w, "hello")
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v \n", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %v \n", name)
+	fmt.Fprintf(w, "Address: %v \n", address)
+}
+
+func main() {
+
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting port at 8083...")
+	if err := http.ListenAndServe(":8083", nil); err != nil {
+		log.Fatal(err)
+	}
+}
